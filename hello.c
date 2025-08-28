989c39ebbc510878ce7258b072effcb233552e3a
Saiyam Gupta <saiyamg369@gmail.com>
2025-08-28 19:09:32 +0530

    First clean commit of hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello My name is Saiyam Gupta\n");
+    printf("I am a Boy");
+    printf("This is an example file");
+    printf("What is going on brother?");
+    return 0;
+}
